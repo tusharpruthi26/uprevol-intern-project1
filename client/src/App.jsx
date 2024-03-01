75056f350c82eef64d9f7995bb5dcd1eaadec964
tusharpruthi26 <122627616+tusharpruthi26@users.noreply.github.com>
2024-03-01 12:43:28 +0530

    make better

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Services from "./pages/services";
 import Products from "./pages/products";
 import Blogs from "./pages/Blogs";
